c36012d2692ab18ea5e086a8ba4eed197ccb7f9e
Mike Constantino <mconstantino@digitalocean.com>
2025-05-14 15:23:40 -0400

    update copilot adapter

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -3,17 +3,17 @@ return {
     'olimorris/codecompanion.nvim',
     config = function()
       require('codecompanion').setup {
-        -- adapters = {
-        --   copilot = function()
-        --     return require('codecompanion.adapters').extend('copilot', {
-        --       schema = {
-        --         model = {
-        --           default = 'claude-3.7-sonnet',
-        --         },
-        --       },
-        --     })
-        --   end,
-        -- },
+        adapters = {
+          copilot = function()
+            return require('codecompanion.adapters').extend('copilot', {
+              schema = {
+                model = {
+                  default = 'claude-3.7-sonnet',
+                },
+              },
+            })
+          end,
+        },
         strategies = {
           chat = {
             adapter = 'copilot',
