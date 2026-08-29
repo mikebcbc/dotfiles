7c748bbe58e8ed2427454e03eef1123cce281380
mikebcbc <mikebconstantino@gmail.com>
2026-08-29 19:54:27 -0400

    add hypr

---
+++ hypr/config/variables.lua
@@ -0,0 +1,16 @@
+-- Hyprland default apps
+
+TERMINAL     = "ghostty"
+FILE_MANAGER = "dolphin"
+BROWSER      = "brave"
+EDITOR       = "nvim"
+CALCULATOR   = "gnome-calculator"
+
+-- Monitors
+MONITOR1 = ""
+MONITOR2 = ""
+MONITOR3 = ""
+PRIMARY_MONITOR = MONITOR1
+
+-- Workspaces
+NUM_WPM = 3 -- Number of workspaces per monitor (Max 10)
