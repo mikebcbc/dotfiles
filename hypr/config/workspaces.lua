2529b9ded6d678b64d166ba37d5302f6964e78c7
mikebcbc <mikebconstantino@gmail.com>
2026-09-10 20:34:45 +0000

    don't bind workspace rules to one monitor

--- hypr/config/workspaces.lua
+++ hypr/config/workspaces.lua
@@ -1,9 +1,9 @@
 -- Workspace rules wiki https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
 -- Named workspaces come from NAMED_WSPACES in variables.lua.
 for _, ws in ipairs(NAMED_WSPACES) do
-    hl.workspace_rule({ workspace = "name:" .. ws, monitor = MONITOR1, persistent = true })
+    hl.workspace_rule({ workspace = "name:" .. ws, persistent = true })
 end
-hl.workspace_rule({ workspace = "name:Gaming", monitor = PRIMARY_MONITOR, default = false })
+hl.workspace_rule({ workspace = "name:Gaming", default = false })
 -- hl.workspace_rule({ workspace = "2", monitor = MONITOR1, default = true, persistent = true })
 -- hl.workspace_rule({ workspace = "3", monitor = MONITOR1, default = true, persistent = true })
 -- hl.workspace_rule({ workspace = "4", monitor = MONITOR2, default = true, persistent = true })
