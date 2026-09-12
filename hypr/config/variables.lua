68e83cba1e8aa2639ced9dec31858d9999f62117
mikebcbc <mikebconstantino@gmail.com>
2026-09-11 20:31:32 -0400

    DP1->DP2

--- hypr/config/variables.lua
+++ hypr/config/variables.lua
@@ -7,7 +7,7 @@ EDITOR = "nvim"
 CALCULATOR = "gnome-calculator"
 
 MONITOR1 = "HDMI-A-2" -- desktop external via HDMI
-MONITOR2 = "DP-1" -- laptop external via USB-C (Alt-DP)
+MONITOR2 = "DP-2" -- laptop external via USB-C (Alt-DP)
 MONITOR3 = ""
 PRIMARY_MONITOR = MONITOR1
 
