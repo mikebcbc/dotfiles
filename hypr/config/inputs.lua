93c2474b8a4e83ee6f767e23651d275aa6e237a3
mikebcbc <mikebconstantino@gmail.com>
2026-09-16 08:24:08 -0400

    add touchpad inputs

--- hypr/config/inputs.lua
+++ hypr/config/inputs.lua
@@ -1,18 +1,23 @@
 -- Input configuration
 
 hl.config({
-    input = {
-        sensitivity = 0.1,
-        accel_profile = "flat",
-		  follow_mouse = 0,
-    },
-    -- Uncomment the section below to enable software cursors; this can help with cursor display or behavior issues
-    -- cursor = {
-    --     no_hardware_cursors = 1,
-    -- },
+	input = {
+		sensitivity = 0.1,
+		accel_profile = "flat",
+		follow_mouse = 0,
+	},
 })
 
--- hl.gesture({ fingers = 4, direction = "horizontal", action = "workspace" })
--- hl.gesture({ fingers = 3, direction = "down",       action = "close" })
--- hl.gesture({ fingers = 3, direction = "up",         action = "fullscreen" })
+-- Faster touchpad only
+hl.device({
+	name = "elan0678:00-04f3:3195-touchpad",
+	sensitivity = 0.25,
+	scroll_factor = 0.2,
+	accel_profile = "adaptive",
+})
+
+-- Touchpad gestures
+hl.gesture({ fingers = 4, direction = "horizontal", action = "workspace" })
+hl.gesture({ fingers = 3, direction = "up", action = "fullscreen" })
+-- hl.gesture({ fingers = 3, direction = "down", action = "close" })
 -- hl.gesture({ fingers = 3, direction = "left",       action = "float" })
